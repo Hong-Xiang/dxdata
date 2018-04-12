c77e90c5b6f01b6b9b7d1ec1f09b0d612902856c
HongXiang <hx.hongxiang@gmail.com>
2018-04-12 09:10:33 +0800

    modified msssim tests

--- tests/python/statistics/image/test_msssim.py
+++ tests/python/statistics/image/test_msssim.py
@@ -6,10 +6,10 @@ from dxl.data.statistics.image import msssim
 class TestMSSSIMCPU(unittest.TestCase):
   def test_basic(self):
     img0 = np.arange(256 * 256).reshape([1, 256, 256, 1]).astype(
-        np.float32) / 256.0
+        np.float32) / 256.0 / 256.0 * 255.0
     img1 = img0.T
     result = msssim(img0, img1, 255, backend='cpu')
-    expected = 0.32705
+    expected = 0.3284
     self.assertEqual(result.shape, (1, ))
     self.assertAlmostEqual(result[0], expected, places=4)
 
@@ -17,9 +17,9 @@ class TestMSSSIMCPU(unittest.TestCase):
 class TestMSSSIMTensorflow(unittest.TestCase):
   def test_basic(self):
     img0 = np.arange(256 * 256).reshape([1, 256, 256, 1]).astype(
-        np.float32) / 256.0
+        np.float32) / 256.0 / 256.0 * 255.0
     img1 = img0.T
     result = msssim(img0, img1, 255, backend='tensorflow')
-    expected = 0.32705
+    expected = 0.3284
     self.assertEqual(result.shape, (1, ))
     self.assertAlmostEqual(result[0], expected, places=4)
