24b2b45d429ebc88bc16699eb0dc96c0e0795a3b
HongXiang <hx.hongxiang@gmail.com>
2018-04-13 17:51:55 +0800

    fix package problem

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup, find_packages
 setup(
     name='dxl-data',
-    version='0.0.5',
+    version='0.0.7',
     description='Data Processing Library.',
     url='https://github.com/Hong-Xiang/dxdata',
     author='Hong Xiang',
     author_email='hx.hongxiang@gmail.com',
     license='MIT',
-    packages=[
-        'dxl.data', 'dxl.data.core', 'dxl.data.core.image',
-        'dxl.data.core.numpy_ops', 'dxl.data.io',
-        'dxl.data.statistics.image',
-        'dxl.data.statistics.utils'
-    ],
+    namespace_packages=['dxl'],
+    packages=find_packages('src/python'),
     package_dir={'': 'src/python'},
     install_requires=['dxl-core', 'dxl-fs', 'pandas==0.20.3'],
     scripts=[],
