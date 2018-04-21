ead31121834f462237378484b2b36229b356d9c0
HongXiang <hx.hongxiang@gmail.com>
2018-04-21 15:47:30 +0800

    add support for none slices

--- src/python/dxl/data/core/persistence/_ndarray.py
+++ src/python/dxl/data/core/persistence/_ndarray.py
@@ -1,5 +1,7 @@
 from ._base import PersistentData, PersistentDataInDataset
 from abc import ABCMeta, abstractmethod, abstractproperty
+import h5py
+import numpy as np
 
 
 class ArrayND:
@@ -25,15 +27,17 @@ class ArrayND:
 
 
 class ArrayNDHDF5(PersistentDataInDataset, ArrayND):
-  def __init__(self, path_file, path_in_dataset, slices):
-    PersistentDataInDataset.__init__(self, path_file, path_in_dataset)
+  def __init__(self, path_file, path_dataset, slices=None):
+    PersistentDataInDataset.__init__(self, path_file, path_dataset)
     ArrayND.__init__(self, None)
     self._slices = slices
 
   def data(self):
-    import h5py
     if self._data is None:
       with h5py.File(self.path) as fin:
-        slices = [slice(*s) for s in self._slices]
-        self._data = fin[self.path_in_dataset][slices]
+        if self._slices is not None:
+          slices = [slice(*s) for s in self._slices]
+          self._data = np.array(fin[self.path_in_dataset][slices])
+        else:
+          self._data = np.array(fin[self.path_in_dataset])
     return self._data
